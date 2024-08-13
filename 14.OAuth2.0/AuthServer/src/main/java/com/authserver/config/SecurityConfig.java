980079d2132d6251d3faeec343e2822a10afa64a
dev-jsshin <dev.jsshin@gmail.com>
2024-08-13 23:40:10 +0900

    chore: Update OAuth2.0 Auth Server (password / jdbc)

--- 14.OAuth2.0/AuthServer/src/main/java/com/authserver/config/SecurityConfig.java
+++ 14.OAuth2.0/AuthServer/src/main/java/com/authserver/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package com.authserver.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -38,10 +37,4 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
-    }
-
 }
