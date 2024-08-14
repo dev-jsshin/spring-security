ab2b60ef597074f5822833e64cbb04b60edbf2cc
dev-jsshin <dev.jsshin@gmail.com>
2024-08-14 23:52:29 +0900

    chore: clean code

--- 14.OAuth2.0/AuthServer/src/main/java/com/authserver/config/AuthServerConfig.java
+++ 14.OAuth2.0/AuthServer/src/main/java/com/authserver/config/AuthServerConfig.java
@@ -13,8 +13,6 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
-import javax.sql.DataSource;
-
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
--- 14.OAuth2.0/ResourceServer/src/main/java/com/resourceserver/config/ResourceServerConfig.java
+++ 14.OAuth2.0/ResourceServer/src/main/java/com/resourceserver/config/ResourceServerConfig.java
@@ -1,6 +1,5 @@
 package com.resourceserver.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,7 +7,6 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
